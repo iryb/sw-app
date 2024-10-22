86b4db3bd10ab932484462bff73f6c875eab02d7
Iryb <iryna.burianovata@gmail.com>
2024-10-22 17:20:09 +0300

    Add React Flow

--- src/components/HeroModal.tsx
+++ src/components/HeroModal.tsx
@@ -5,7 +5,15 @@ import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import {
+  addEdge,
+  applyNodeChanges,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
 
 type HeroModalProps = {
   name: string;
@@ -14,6 +22,8 @@ type HeroModalProps = {
   open: boolean;
   onClose: () => void;
 };
+
+const initialEdges = [{ id: "e1-1", source: "0", target: "0" }];
 
 export const HeroModal = ({
   name,
@@ -26,9 +36,34 @@ export const HeroModal = ({
   const [films, setFilms] = useState<Film[]>([]);
   const [starships, setStarships] = useState<Starship[]>([]);
 
+  const initialNodes = [
+    { id: "0", position: { x: 0, y: 0 }, data: { label: <p>{name}</p> } },
+  ];
+
+  const [nodes, setNodes] = useNodesState(initialNodes);
+  const [edges, setEdges] = useEdgesState(initialEdges);
+
   const handleClose = () => {
     onClose();
   };
+
+  const addNode = useCallback((id: string, position: any, data: any) => {
+    setNodes((nds) => [
+      ...nds,
+      {
+        id,
+        position,
+        data,
+      },
+    ]);
+  }, []);
+
+  const addNewEdge = useCallback(
+    (id: string, source: string, target: string) => {
+      setEdges((eds) => addEdge({ id, source, target }, eds));
+    },
+    []
+  );
 
   useEffect(() => {
     getPersonFilms(id)
@@ -39,14 +74,52 @@ export const HeroModal = ({
   useEffect(() => {
     if (starshipsIds.length > 0) {
       getPersonStarships(starshipsIds)
-        .then(({ results }) => setStarships(results))
+        .then(({ results }) => {
+          setStarships(results);
+        })
         .catch((e) => setError(e.message));
     }
   }, [starshipsIds]);
 
+  useEffect(() => {
+    setNodes(initialNodes);
+    setEdges(initialEdges);
+  }, [id]);
+
+  useEffect(() => {
+    films.forEach(({ id, title, starships: filmStarships }) => {
+      addNode(
+        `film-n-${id}`,
+        { x: Math.random() * 500, y: 100 },
+        { label: <p>{title}</p> }
+      );
+      addNewEdge(`film-e-${id}`, "0", `film-n-${id}`);
+
+      starships
+        .filter((starship) => filmStarships.includes(starship.id))
+        .forEach((starship) => {
+          addNode(
+            `starship-n-${starship.id}`,
+            { x: Math.random() * 500, y: 200 },
+            { label: <p>{starship.name}</p> }
+          );
+          addNewEdge(
+            `starship-e-${id}-${starship.id}`,
+            `film-n-${id}`,
+            `starship-n-${starship.id}`
+          );
+        });
+    });
+  }, [addNewEdge, addNode, films, starships]);
+
   return (
     <Dialog onClose={handleClose} open={open}>
       <DialogTitle>{name}</DialogTitle>
+      {nodes.length > 0 && (
+        <div style={{ width: "500px", height: "500px" }}>
+          <ReactFlow nodes={nodes} edges={edges} fitView />
+        </div>
+      )}
       {films && (
         <DialogContent>
           {films.map((f) => (
